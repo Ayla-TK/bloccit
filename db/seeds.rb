d7d68c0a63041c56e683267a41a2e910a227bd62
Ayla Tolosa-Kline <AylaMPH@gmail.com>
2017-10-06 16:57:21 +0300

    Checkpoint 27| Rails: Authorization

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,9 +42,25 @@ topics = Topic.all
 
    user = User.first
    user.update_attributes!(
-     email: 'aylaMPH@gmail.com', 
+     email: 'aylaMPH@gmail.com',
      password: 'helloworld'
    )
+
+# Create an admin user
+    admin = User.create!(
+      name:     'Admin User',
+      email:    'admin@example.com',
+      password: 'helloworld',
+      role:     'admin'
+    )
+
+# Create a member
+    member = User.create!(
+      name:     'Member User',
+      email:    'member@example.com',
+      password: 'helloworld'
+    )
+
  puts "Seed finished"
  puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
